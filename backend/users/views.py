6232c0f3977afeec3f8de9bd9b75bb02cad9cb82
=anxxooo <mm_touat@esi.dz>
2024-12-15 10:33:59 +0100

    added auth backend

---
+++ backend/users/views.py
@@ -0,0 +1,49 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .decorators import role_required
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        print("i aù here")
+        serializer.save()
+        user = CustomUser.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data})
+    return Response(serializer.errors, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(CustomUser, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    print("success")
+    return Response({'token': token.key})
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed!")
+
+@api_view(['GET'])
+@role_required(allowed_roles=["Admin", "Médecin"])
+def test(request):
+    return Response({
+        "message": "You are allowed",
+        "user": request.user.username,
+        "role": request.user.role,
+    })
+
