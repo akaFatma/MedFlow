b417e8f9d683822beccc514cb51bdc5e13abbd4f
akaFatma <mf_salhi@esi.dz>
2024-12-15 09:24:17 +0100

    added HTTP interceptor and sanitized user inputs

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';  // Your routes file
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { routes } from './app/app.routes';  
+import { AuthInterceptor } from './app/services/auth.interceptor'
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(), 
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideRouter(routes)]
 })
   .catch(err => console.error(err));
