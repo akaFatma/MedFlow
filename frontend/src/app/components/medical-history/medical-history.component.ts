b955701659a4088b216e0fa85de2598f290cd720
akaFatma <mf_salhi@esi.dz>
2024-12-24 23:20:37 +0100

    added consultation-history services

--- frontend/src/app/components/medical-history/medical-history.component.ts
+++ frontend/src/app/components/medical-history/medical-history.component.ts
@@ -1,23 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
+import { ConsultationHistoryService } from '../../services/consultation-history';
+import { Injectable } from '@angular/core';
 
 interface Consultation {
-  
-  date : string ;
-  doctor : string ;
-  specialty : string ;
-
+  date: string;
+  doctor: string;
+  specialty: string;
 }
-
-
+@Injectable({
+  providedIn: 'root'  
+})
 
 @Component({
   selector: 'app-medical-history',
-  imports: [CommonModule,NgFor],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './medical-history.component.html',
   styleUrl: './medical-history.component.scss'
 })
+
+/*export class MedicalHistoryComponent implements OnInit {
+  consultations: Consultation[] = [];
+  errorMessage: string = '';
+
+  constructor(private consultationService: ConsultationHistoryService) {}
+
+  ngOnInit(): void {
+    this.loadConsultations();
+  }
+
+  loadConsultations(): void {
+    this.consultationService.getConsultationHistory().subscribe({
+      next: (data) => {
+        this.consultations = data;
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+        console.error('Error fetching consultations:', error);
+      }
+    });
+  }
+}*/
+
 export class MedicalHistoryComponent {
   consultations: Consultation[] = [
     { date: '2024-12-20', doctor: 'Dr. TOUAT', specialty: 'Cardiologue' },
@@ -25,3 +50,10 @@ export class MedicalHistoryComponent {
     { date: '2024-12-05', doctor: 'Dr. FELKIR', specialty: 'Neurologue' }
   ];
 }
+
+
+
+
+
+
+
