a3c47bd78231aa89caa340a3356404d611f595a5
akaFatma <mf_salhi@esi.dz>
2024-12-25 01:08:36 +0100

    started with the bienvenu component

---
+++ frontend/src/app/components/bienvenu-component/bienvenu-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenu-component',
+  imports: [],
+  templateUrl: './bienvenu-component.component.html',
+  styleUrl: './bienvenu-component.component.scss'
+})
+export class BienvenuComponentComponent {
+
+}
