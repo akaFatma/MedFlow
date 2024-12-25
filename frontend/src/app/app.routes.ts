e3a1feee466332fbb15338a8ab406a2d7ddaa3a8
akaFatma <mf_salhi@esi.dz>
2024-12-25 16:34:36 +0100

    finished consulterDPI patient , needs stylin forresponsivenes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { AddDPIComponent } from './pages/add-dpi/add-dpi.component';
-import { MedicalHistoryComponent } from './components/medical-history/medical-history.component';
-import { PersonalInfoCardComponent } from './components/personal-info-card/personal-info-card.component';
-import { BienvenuComponentComponent } from './components/bienvenu-component/bienvenu-component.component';
+import { ConsulterDPIPovPatientComponent } from './pages/consulter-dpipov-patient/consulter-dpipov-patient.component';
+
 
 export const routes: Routes = [
-  { path: 'users/login', component: LoginPageComponent },
-  { path: 'add-dpi', component: AddDPIComponent },
-  { path: 'ConsulterDPI', component: MedicalHistoryComponent },
-  { path: 'ConsulterPersonalInfo', component: PersonalInfoCardComponent },
-  { path: 'bienvenue', component: BienvenuComponentComponent },
+  { path: 'test', component: ConsulterDPIPovPatientComponent },
+ 
 
 ];
