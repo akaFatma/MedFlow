d8b08fed229bcc1ccb3dafb75b7d90c8008ec8af
akaFatma <mf_salhi@esi.dz>
2024-12-25 18:58:23 +0100

    also fixed dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ConsulterDPIPovPatientComponent } from './pages/consulter-dpipov-patient/consulter-dpipov-patient.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 
 export const routes: Routes = [
   { path: 'test', component: ConsulterDPIPovPatientComponent },
- 
+  { path: 'testt', component: SideBarComponent},
 
 ];
