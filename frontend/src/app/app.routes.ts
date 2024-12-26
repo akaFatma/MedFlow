abd0cc94ecdb58a643bd24f037b309a85468d663
akaFatma <mf_salhi@esi.dz>
2024-12-26 11:01:57 +0100

    finished search bar component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { ConsulterDPIPovPatientComponent } from './pages/consulter-dpipov-patient/consulter-dpipov-patient.component';
+<<<<<<< Updated upstream
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { MedecinTableComponent } from './components/medecin-table/medecin-table.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 export const routes: Routes = [
   { path: 'test', component: ConsulterDPIPovPatientComponent },
   { path: 'testt', component: SideBarComponent},
-  {path: 'testtt', component: MedecinTableComponent},
+{path : 'ff' , component : SearchBarComponent}
+=======
+
+export const routes: Routes = [
+  { path: 'test', component: ConsulterDPIPovPatientComponent },
+>>>>>>> Stashed changes
 
 ];
