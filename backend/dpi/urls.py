369c1f1e3dbf1976c6bbd76202778a2a76b7b2b1
=anxxooo <mm_touat@esi.dz>
2024-12-26 17:47:35 +0100

    wip

--- backend/dpi/urls.py
+++ backend/dpi/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('patients/qrcode', views.get_dpi_by_qr, name='qr'),
     path('test', views.qr_code, name='qr'), #pour generer le code qr, probablement sera fusionné avec creer dpi
     path('create', views.create_patient_with_dpi, name='creer'),#utilisée juste pour tester
-        path('ordonnances/<int:id>/valider/', views.valider_ordonnance, name='valider_ordonnance'),
+    path('ordonnances/<int:id>/valider/', views.valider_ordonnance, name='valider_ordonnance'),
     path('distributions/', views.distribuer_medicament, name='distribuer_medicament'),
 ]
