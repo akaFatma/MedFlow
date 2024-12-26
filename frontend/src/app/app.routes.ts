c75e3886a91527195719cb302d27a359348e9e10
akaFatma <mf_salhi@esi.dz>
2024-12-26 20:38:49 +0100

    updated Medecin landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { ConsulterDPIPovPatientComponent } from './pages/consulter-dpipov-patient/consulter-dpipov-patient.component';
-import { SuccessNotifComponent } from './components/success-notif/success-notif.component';
-import { BienvenuComponentComponent } from './components/bienvenu-component/bienvenu-component.component';
+import { MedecinLandingPageComponent } from './pages/medecin-landing-page/medecin-landing-page.component';
 
 export const routes: Routes = [
-  { path: 'test', component : BienvenuComponentComponent },
+  { path: 'test', component : MedecinLandingPageComponent },
 
 ];
