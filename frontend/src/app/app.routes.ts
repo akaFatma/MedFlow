c94fcaf0273562edf25f68df3c21ee30de054204
akaFatma <mf_salhi@esi.dz>
2024-12-27 14:45:21 +0100

    added unauthorized page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RoleGuard } from './services/role.guard';
 import { MedecinLandingPageComponent } from './pages/medecin-landing-page/medecin-landing-page.component';
+import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorized-page.component';
 
 export const routes: Routes = [
   { path: 'LandingPage', component : MedecinLandingPageComponent },
   { path: 'medecin-landing', component: MedecinLandingPageComponent, canActivate: [RoleGuard], data: { role: 'medecin' }},
+  { path: 'unauthorized', component: UnauthorizedPageComponent },
   { path: '**', redirectTo: '/login' } 
 ];
