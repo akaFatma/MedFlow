c4ce5f1943fef4b1e3035de5dc3d9f5579eb4604
akaFatma <mf_salhi@esi.dz>
2025-01-28 20:11:49 +0100

    added add dpi test

--- frontend/src/app/pages/radiologue/radiologue.component.ts
+++ frontend/src/app/pages/radiologue/radiologue.component.ts
@@ -1,11 +1,11 @@
-import { HeaderComponent } from '../../components/header/header.component';
+
 import { RadioTableComponent } from '../../components/radio-table/radio-table.component';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-radiologue',
-  imports: [HeaderComponent, RadioTableComponent], // Updated to radio-table
+  imports: [ RadioTableComponent], // Updated to radio-table
   templateUrl: './radiologue.component.html',
   styleUrls: ['./radiologue.component.scss'],
 })
